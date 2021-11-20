2c54a8adcb0968e392bc4a7e98930fa2e55c2c91
caroldf07 <carolina.df@outlook.com>
2021-11-19 21:25:19 -0300

    aula revisão - parcial

---
+++ src/main/java/com/example/demo/dto/RespostaCompletaPlanetas.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+public class RespostaCompletaPlanetas {
+
+    private int count;
+    private String next;
+    private String previous;
+    private List<ResultadosPlanetas> resultadosPlanetas;
+}
---
+++ src/main/java/com/example/demo/dto/ResultadosPlanetas.java
@@ -0,0 +1,7 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+public class ResultadosPlanetas {
+    List<PlanetaDto> planetaDto;
+}
